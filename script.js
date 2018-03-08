aa4116bff775ccd4f36b5b4a71668f0dfd598fd2
sachk <sachakorban@gmail.com>
2018-03-08 21:37:41 +1100

    Add scrolling

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var x = Math.floor(Math.random() * 3);
 $(document).ready(function(){
-    
+     
 	'use strict';
         if (x === 0){
             $( ".quotetext" ).append( "Greatings" );
@@ -17,32 +17,22 @@ $(document).ready(function(){
     
     $(".startbutton").click(Start);
 
-    $(".next1button").click(NextOne);
-
-    $(".backbutton").click(BackToOne);
 })
 
 function Start(){
     $(".startdiv").fadeOut(500);
+    $(".startdiv").remove();
     $(".firstdiv").delay(500).animate({
         opacity: '1',
         marginLeft: ($(window).width() - $(".firstdiv").width()) / 2 
     })
+
+    $(function() {
+        $.scrollify({
+          section : ".page",
+        });
+      });
+  
 }
 
-function NextOne(){
-    $(".firstdiv").fadeOut(500);
-    $(".backbutton").delay(500).fadeIn();
-    $(".nextdivone").delay(500).animate({
-        opacity: '1',
-        marginLeft: ($(window).width() - $(".nextdivone").width()) / 2 
-    });
-}
-
-function BackToOne(){
-    $(".seconddiv").fadeOut(500);
-    $(".firstdiv").delay(500).fadeIn();
-    $(".backbutton").fadeOut();
-
-}
 
