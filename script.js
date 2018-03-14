c86c7523e0e0000ef8b0cdea3a203e2b54f45453
sachk <sachakorban@gmail.com>
2018-03-14 12:37:55 +1100

    Redo pretty much everything

--- script.js
+++ script.js
@@ -1,39 +1,6 @@
-var x = Math.floor(Math.random() * 3);
-$(document).ready(function(){
-     
-	'use strict';
-        if (x === 0){
-            $( ".quotetext" ).append( "\"Greatings\"" );
-        }
-
-        if (x === 1) {
-            $( ".quotetext" ).append( "\"Yes is of an project\"" );
-        }
-        if (x === 2) {
-            $( ".quotetext" ).append( "\"Hello\"" );
-        }
-
-    $("body").fadeIn(500);
-    
-    $(".startbutton").click(Start);
-
-})
-
-function Start(){
-    $(".startdiv").fadeOut(500);
-    $(".startdiv").remove();
-    $(".firstdiv").delay(500).animate({
-        opacity: '1',
-        marginLeft: ($(window).width() - $(".firstdiv").width()) / 2 
-    })
-    $(".page").fadeIn(500);
-    $(".pagination").fadeIn(500);
-    pagination();
-});
-
-function pagination() {
+$(function() {
   $.scrollify({
-		section:".panel",
+    section:".panel",
     scrollbars:false,
     before:function(i,panels) {
 
@@ -70,3 +37,19 @@ function pagination() {
     }
   });
 });
+
+var x = Math.floor(Math.random() * 3);
+$(document).ready(function(){
+     
+  'use strict';
+        if (x === 0){
+            $( ".quotetext" ).append( "\"Greatings\"" );
+        }
+
+        if (x === 1) {
+            $( ".quotetext" ).append( "\"Yes is of an project\"" );
+        }
+        if (x === 2) {
+            $( ".quotetext" ).append( "\"Hello\"" );
+        }
+})
